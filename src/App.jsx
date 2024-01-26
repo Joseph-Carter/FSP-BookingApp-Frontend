be0f537e75fcfb2f484eea44245708be83fccb72
Nicole Slater <nicoleslater@pursuit.org>
2024-01-25 21:31:12 -0500

    Updated Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import UserList from "./Pages/UserList";
+import UserDetails from "./Pages/UserDetails";
+import BookingList from "./Pages/BookingList";
+import BookingDetails from "./Pages/BookingDetails";
+import RoomList from "./Pages/RoomList";
+import RoomDetails from "./Pages/RoomDetails";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+          <Route path="/bookings" element={<BookingList />} />
+          <Route path="/bookings/:id" element={<BookingDetails />} />
+          <Route path="/rooms" element={<RoomList />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
