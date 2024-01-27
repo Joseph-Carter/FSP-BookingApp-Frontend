d303ebc1e4332330214c951ffd43fde01fd11013
Joseph-Carter <1joseph1carter@gmail.com>
2024-01-26 22:26:54 -0500

    commit before a pull

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function NavBar() {
+const NavBar = () => {
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="navbar-container">
+      <Link to={`/rooms`}>Rooms</Link>
+      <h2 className="recentPosts navlink">
+        <Link to={`/bookings`}>Recent Bookings</Link>
+      </h2>
+      <h2 className="newPosts navlink">
+        <Link to={`/bookings/new`}>New Booking</Link>
+      </h2>
+      <h2 className="logout navlink">
+        <Link to={`/`}>Log Out</Link>
+      </h2>
+    </nav>
+  );
+};
+
+export default NavBar;
