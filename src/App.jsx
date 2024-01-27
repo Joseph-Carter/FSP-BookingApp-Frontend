056b2802cfe49bfed0dbc4c02bc57945c3195b3c
DJones <dwaynejones@pursuit.com>
2024-01-27 14:31:01 -0500

    current configuration changes according to app requirements logic incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate, } from "react-router-dom";
 import { AuthProvider } from "./Components/UserAuth/UserContext";
 import NavBar from "./Components/NavBar";
-<<<<<<< HEAD
 import EventSpaceList from "./Components/EventSpaceList";
 import EventSpaceDetails from "./Components/EventSpaceDetails"; 
 import EventSpaceNewForm from "./Components/EventSpaceNewForm"
-=======
->>>>>>> ce3e867b58b681f20db81f33defb80af22e0008f
 import BookingList from "./Components/BookingList";
 import BookingDetails from "./Components/BookingDetails";
 import BookingNewForm from "./Components/BookingNewForm";
@@ -16,13 +13,16 @@ import Auth from "./Pages/Auth";
 
 
 function App() {
+
   return (
     <div className="App">
       <Router>
         <AuthProvider>
-<<<<<<< HEAD
         <NavBar />
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Auth />} />
+          <Route path="/signup" element={<Auth />} />
           <Route path="/bookings" element={<BookingList />} />
           <Route path="/booking/:id" element={<BookingDetails />} />
           <Route path="/booking/:new" element={<BookingNewForm />} />
@@ -33,19 +33,6 @@ function App() {
           <Route path="/room/:edit" element={<EventSpaceNewForm />} />
           <Route path="*" element={<Four0Four />} />
         </Routes>
-=======
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/login" element={<Auth />} />
-            <Route path="/signup" element={<Auth />} />
-            <Route path="/bookings" element={<BookingList />} />
-            <Route path="/bookings/:id" element={<BookingDetails />} />
-            {/* <Route path="/rooms" element={<RoomList />} /> */}
-            {/* <Route path="/rooms/:id" element={<RoomDetails />} /> */}
-            <Route path="*" element={<Four0Four />} />
-          </Routes>
->>>>>>> ce3e867b58b681f20db81f33defb80af22e0008f
         </AuthProvider>
       </Router>
     </div>
--- src/Components/BookingNewForm.jsx
+++ src/Components/BookingNewForm.jsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import EventSpacesDropdown from './EventSpacesDropdown';
-
-const API = import.meta.env.VITE_API_URL;
-
-=======
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const API = import.meta.env.VITE_API_URL;
 
 // allows booking of event space
->>>>>>> ce3e867b58b681f20db81f33defb80af22e0008f
 export default function BookingNewForm() {
   const [newBooking, setNewBooking] = useState({
     name: "",
@@ -34,16 +25,6 @@ export default function BookingNewForm() {
       },
       body: JSON.stringify(newBooking),
     })
-<<<<<<< HEAD
-    .then(res => res.json())
-    .then(data => {
-      console.log('Booking created:', data);
-      navigate("/bookings");
-    })
-    .catch((err) => {
-      console.error('Error:', err);
-    });
-=======
       .then((res) => res.json())
       .then((data) => {
         console.log("Booking created:", data);
@@ -52,7 +33,6 @@ export default function BookingNewForm() {
       .catch((err) => {
         console.error("Error:", err);
       });
->>>>>>> ce3e867b58b681f20db81f33defb80af22e0008f
   };
 
   const handleChange = (e) => {
@@ -60,7 +40,6 @@ export default function BookingNewForm() {
   };
 
   return (
-<<<<<<< HEAD
     <form onSubmit={handleSubmit}>
       <input type="text" name="name" placeholder="Event Name" onChange={handleChange} required />
       <input type="text" name="user_id" placeholder="Client ID" onChange={handleChange} required />
@@ -72,49 +51,6 @@ export default function BookingNewForm() {
       <EventSpacesDropdown />
       <button type="submit">Submit Booking</button>
       <button type="button" onClick={() => navigate("/bookings")}>Cancel Booking</button>
-=======
-    <form>
-      <input
-        type="text"
-        name="name"
-        placeholder="Event Name"
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="number"
-        name="user_id"
-        placeholder="Client Name"
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="number"
-        name="room_id"
-        placeholder="Room Name"
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="dateTime"
-        name="start_date"
-        onChange={handleChange}
-        required
-      />
-      <input type="dateTime" name="end_date" onChange={handleChange} required />
-      <input
-        type="number"
-        name="attendees"
-        placeholder="Number of Attendees Name"
-        onChange={handleChange}
-      />
-      <textarea
-        name="special_requirements"
-        placeholder="Special aRequirements"
-        onChange={handleChange}
-      ></textarea>
-      <button name="submit">Submit Booking</button>
->>>>>>> ce3e867b58b681f20db81f33defb80af22e0008f
     </form>
   );
 }
