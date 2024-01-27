b6d506fb15aa23506f1b2a1cf364621cb278d7bc
DJones <dwaynejones@pursuit.com>
2024-01-26 22:08:39 -0500

    new comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
-import UserList from "./Pages/UserList";
+// import UserList from "./Pages/UserList";
 import UserDetails from "./Components/UserDetails";
-import BookingList from "./Pages/BookingList";
-import BookingDetails from "./Pages/BookingDetails";
-import RoomList from "./Pages/RoomList";
-import RoomDetails from "./Pages/RoomDetails";
+import BookingList from "./Components/BookingList";
+import BookingDetails from "./Components/BookingDetails";
+// import RoomList from "./Pages/RoomList";
+// import RoomDetails from "./Pages/RoomDetails";
 import Four0Four from "./Pages/Four0Four";
 
 function App() {
@@ -16,12 +16,12 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
+          {/* <Route path="/users" element={<UserList />} /> */}
           <Route path="/users/:id" element={<UserDetails />} />
           <Route path="/bookings" element={<BookingList />} />
           <Route path="/bookings/:id" element={<BookingDetails />} />
-          <Route path="/rooms" element={<RoomList />} />
-          <Route path="/rooms/:id" element={<RoomDetails />} />
+          {/* <Route path="/rooms" element={<RoomList />} /> */}
+          {/* <Route path="/rooms/:id" element={<RoomDetails />} /> */}
           <Route path="*" element={<Four0Four />} />
         </Routes>
       </Router>
