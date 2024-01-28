8ac200b9887babfad4075126a2c04f4d359d50e2
Nicole Slater <nicoleslater@pursuit.org>
2024-01-28 11:28:52 -0500

    Updated CSS for EventSpace Card and corrected Fetch statement

--- src/Components/EventSpacesDropdown.jsx
+++ src/Components/EventSpacesDropdown.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+const API = import.meta.env.VITE_API_URL;
+
 export default function EventSpacesDropdown({ preselectedSpaceId }) {
     const [eventSpaces, setEventSpaces] = useState([]);
 
     useEffect(() => {
-        fetch('/api/eventspaces')
+        fetch(`/${API}/eventspaces`)
             .then(response => response.json())
             .then(data => setEventSpaces(data))
             .catch(error => console.error('Error:', error));
